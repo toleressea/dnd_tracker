7d3f525001dee6550ac66d3f56c2c1ef1e9d8841
Daniel Carcich <danielreid@gmail.com>
2017-05-17 20:43:16 -0400

    Flesh out more database queries with "tests"

--- database.py
+++ database.py
@@ -12,32 +12,78 @@ class Database():
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 campaign_id INTEGER)''')
         self.c.execute('''CREATE TABLE IF NOT EXISTS characters (
-                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                campaign_id INTEGER,
                 fight_id INTEGER, 
                 name STRING, 
                 player INTEGER,  
-                hp INTEGER, 
-                damage_taken INTEGER, 
+                current_hp INTEGER,
+                max_hp INTEGER,
                 state STRING, 
-                position STRING, 
+                position STRING,
                 high_miss_ar INTEGER, 
                 low_hit_ar INTEGER)''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS attacks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                campaign_id INTEGER,
+                fight_id INTEGER,
+                source_id INTEGER,
+                target_id INTEGER,
+                attack_roll INTEGER,
+                hit INTEGER,
+                damage INTEGER)''')
         self.conn.commit()
 
     def addCampaign(self):
         self.c.execute("INSERT INTO campaigns (id) VALUES (NULL)")
         self.conn.commit()
-        return self.c.execute("SELECT (id) FROM campaigns ORDER BY id DESC LIMIT 1").fetchone()[0]
+        return self.c.lastrowid
 
     def addFight(self, campaign_id):
         self.c.execute("INSERT INTO fights (campaign_id) VALUES ({0})".\
                        format(campaign_id))
         self.conn.commit()
-        return self.c.execute("SELECT (id) FROM fights ORDER BY id DESC LIMIT 1").fetchone()[0]
+        return self.c.lastrowid
 
-    def addCharacter(self, fight_id, name, player, count = 1, hp = 0):
-        for i in range(0, count):
-            self.c.execute('''INSERT INTO characters (fight_id, name, player, hp, damage_done, damage_taken)
-                              VALUES ({0}, '{1}', {2}, {3}, 0, 0)'''.\
-                           format(fight_id, name + str(i), int(player), hp))
+    def addCharacter(self, campaign_id, fight_id, name, player, current_hp, max_hp, count = 1):
+        sqlString = '''INSERT INTO characters (campaign_id, fight_id, name, player, 
+                       current_hp, max_hp, high_miss_ar, low_hit_ar)
+                       VALUES ({0}, {1}, '{2}', {3}, {4}, {5}, 0, 0)'''
+        if count > 1:
+            for i in range(0, count):
+                self.c.execute(sqlString.format(campaign_id, fight_id, name + ' ' + str(i), int(player),
+                                                current_hp, max_hp))
+        else:
+            self.c.execute(sqlString.format(campaign_id, fight_id, name, int(player),
+                                            current_hp, max_hp))
         self.conn.commit()
+
+    def addAttack(self, campaign_id, fight_id, source_character_id, target_character_id,
+                  attack_roll, hit, damage):
+        self.c.execute('''INSERT INTO attacks (campaign_id, fight_id, source_id, 
+                          target_id, attack_roll, hit, damage)
+                          VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6})'''. \
+                       format(campaign_id, fight_id, source_character_id, target_character_id,
+                              attack_roll, int(hit), damage))
+        self.conn.commit()
+
+    def getCharacter(self, name):
+        return self.c.execute("SELECT id FROM characters WHERE name='{0}'".format(name)).fetchone()[0]
+
+    def getCharacterDamageTaken(self, character_id):
+        return self.c.execute('SELECT sum(damage) FROM attacks WHERE target_id={0}'.format(character_id)).fetchone()[0]
+
+    def getCharacterDamageDone(self, character_id):
+        return self.c.execute('SELECT sum(damage) FROM attacks WHERE source_id={0}'.format(character_id)).fetchone()[0]
+
+    def countCharacterHits(self, character_id):
+        return self.c.execute('SELECT count(*) FROM attacks WHERE source_id={0} AND hit=1'.format(character_id)).fetchone()[0]
+
+    def countCharacterMisses(self, character_id):
+        return self.c.execute('SELECT count(*) FROM attacks WHERE source_id={0} AND hit=0'.format(character_id)).fetchone()[0]
+
+    def getFighters(self, fight_id):
+        return self.c.execute('''SELECT id, name, player, current_hp, max_hp, state, position,
+                              high_miss_ar, low_hit_ar
+                              FROM characters WHERE fight_id={0} ORDER BY id ASC'''.\
+                              format(fight_id)).fetchall()
--- testDatabase.py
+++ testDatabase.py
@@ -1,7 +1,36 @@
+import os
 from database import Database
+
+try:
+    os.remove('data.db')
+except:
+    pass
 
 d = Database();
 d.createTables()
 c = d.addCampaign()
 f = d.addFight(c)
-ct = d.addCharacter(f, 'testName', False, 5)
+d.addCharacter(c, f, 'Tol Eressea', True, 10, 10, 1)
+ct = d.getCharacter('Tol Eressea')
+d.addCharacter(c, f, 'Bad Guy', False, 0, 0, 5)
+ctt = d.getCharacter('Bad Guy 2')
+for i in range(0, 3):
+    d.addAttack(c, f, ct, ctt, 20, True, 15)
+for i in range(0, 7):
+    d.addAttack(c, f, ct, ctt, 10, False, 0)
+print d.getCharacterDamageTaken(ctt)
+print d.getCharacterDamageDone(ct)
+print d.countCharacterHits(ct)
+print d.countCharacterMisses(ct)
+fighters = d.getFighters(f)
+s = ''
+for ft in fighters:
+    for i in range(0, 8):
+        s += str(ft[i]).ljust(15)
+    s += str(d.getCharacterDamageDone(ft[0])).ljust(15)
+    s += str(d.getCharacterDamageTaken(ft[0])).ljust(15)
+    hits = d.countCharacterHits(ft[0])
+    misses = d.countCharacterMisses(ft[0])
+    s += '{0}/{1}'.format(hits, misses).ljust(15)
+    s += '\n'
+print s
